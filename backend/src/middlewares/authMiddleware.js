0b471e1437474c3619827051d648f7171de97226
Exequiel <exequiel.miranda245@gmail.com>
2026-06-23 07:54:41 -0600

    23626

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jsonwebtoken from "jsonwebtoken";
-import { confiig } from "../../config.js";
+import { config } from "../../config.js";
 
 export const validateAuthCookie = (allowedTypes = []) => {
     return (req, res, next) => {
